d030ccc58eeca39560967408ffd9712201cbd9ca
lawlessowl <erokhin-maksim@bk.ru>
2024-02-01 19:55:41 +0300

    refactor: сhanging the server structure

--- server/index.js
+++ server/index.js
@@ -1,23 +1,37 @@
-const expres = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const app = expres();
-
-const route = require('./route')
-
-app.use(cors({ origin: "*" }));
-app.use(route)
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
     cors: {
-        origin: '*',
-        methods: ["GET", "POST"]
+        origin: '*'
     }
 })
 
-server.listen(5000, () => {
-    console.log('server ok')
+const log = console.log;
+
+const registerMessageHandlers = require('./handlers/messageHandlers')
+const registerUserHandlers = require('./handlers/userHandlers')
+
+const onConnection = (socket) => {
+    log('User connected')
+
+    const { roomId } = socket.handshake.query
+
+    socket.roomId = roomId
+
+    socket.join(roomId)
+
+    registerMessageHandlers(io, socket)
+    registerUserHandlers(io, socket)
+
+    socket.on('disconnect', () => {
+        log('User disconnected')
+        socket.leave(roomId)
+    })
+}
+
+io.on('connection', onConnection)
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+    console.log(`Server ready. Port: ${PORT}`)
 })
+
