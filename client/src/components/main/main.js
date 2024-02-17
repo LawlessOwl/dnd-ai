8e8142a5dbe7268af54d61f00a2df654c28238d6
lawlessowl <erokhin-maksim@bk.ru>
2024-02-17 20:00:06 +0300

    refactor: going through the array with map

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -12,18 +12,36 @@ const MessageBody = (props) => {
 
 const UserChatBody = (props) => {
     return (
-        <div className={style.userText}>
-            <img src={props.icon} alt="user-avatar" className={style.userAvatar}/>
-            <MessageBody nickname='Alisa' message='Hello'/>
-        </div>
+        <img src={props.icon} alt="user-avatar" className={style.userAvatar}/>
     )
 }
 
 export const Main = () => {
+    let userInfo = [
+        {
+            nickname:'Alisa',
+            icon: 'https://sun9-8.userapi.com/impg/f0eyk3phbS_NZfavSPK0OGoef_xp_EHZsdCvOg/3PX1FcEp2oI.jpg?size=1280x1280&quality=96&sign=8f9c2a0d5b71315c28176129d2ec2981&type=album',
+            message: 'Hello'
+        }
+    ]
+    let UserIcon = userInfo.map((user) => {
+        return (
+            <UserChatBody icon={user.icon}/>)
+    })
+    
+    let UserItem = userInfo.map((user) => {
+        return (
+            <MessageBody nickname={user.nickname} message={user.message}/>)
+    })
+    console.log(UserItem)
+    
     return (
         <main className={style.main}>
           <div className={style.chatBody}>
-              <UserChatBody icon='https://sun9-8.userapi.com/impg/f0eyk3phbS_NZfavSPK0OGoef_xp_EHZsdCvOg/3PX1FcEp2oI.jpg?size=1280x1280&quality=96&sign=8f9c2a0d5b71315c28176129d2ec2981&type=album'/>
+              <div className={style.userText}>
+                { UserIcon }
+                { UserItem }
+              </div>
           </div>
           <div className={style.chatInput}>
               <textarea name="" id="" cols="30" rows="10" placeholder="Input a message" className={style.messageTextArea}></textarea>
