0cf6b3624dc321ae73d4ff1ad2f34d9e29390213
lawlessowl <erokhin-maksim@bk.ru>
2025-05-31 17:48:42 +0300

    refactor: migrate from CRA to Vite

---
+++ dnd-ai/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Body from './components/body/body'
+import Sidebar from './components/body/sidebar/sidebar'
+import Header from './components/header/header'
+
+const App = () => {
+
+  return (
+   <>
+    <Header/>
+    <Body/>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ dnd-ai/src/components/body/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+      <button>Home</button>
+      <button>Profile</button>
+      <button>Chats</button>
+      <button>Settings</button>
+      <button>Logout</button>
+    </div>
+  )
+}
+
+export default Sidebar
